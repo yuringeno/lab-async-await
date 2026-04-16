7adcb2ec17cf90f59e5e53c78da48f9fb94f3812
yuringeno <nicolecheptoo152@gmail.com>
2026-04-16 14:09:59 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your code here!
+async function fetchPosts() {
+  try {
+    
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await response.json();
+
+    displayPosts(posts);
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+}
+
+function displayPosts(posts) {
+  const ul = document.getElementById('post-list');
+
+  posts.forEach(post => {
+    const li = document.createElement('li');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = post.title;
+
+    const p = document.createElement('p');
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
+}
+
+fetchPosts();
+
+
